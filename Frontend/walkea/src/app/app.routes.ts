985a9ad379c3e6017b4e13a54cde02f6832cdefa
Rocky0524 <kenbalboa05@gmail.com>
2026-02-20 16:56:16 +0100

    componente bienvenida
    creo y configuro componente de la pantalla de bienvenida y ademas retoco unos detalles de los demas codigos

--- Frontend/walkea/src/app/app.routes.ts
+++ Frontend/walkea/src/app/app.routes.ts
@@ -1,15 +1,19 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login'; 
 import { Registro } from './registro/registro'; 
+import { BienvenidaComponent } from './bienvenida/bienvenida'; 
 
 export const routes: Routes = [
 
   // 1. Ruta por defecto: Si el usuario entra a la raíz, va directo al login
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/bienvenida', pathMatch: 'full' },
   
   // 2. Ruta para cargar la pantalla de login
   { path: 'login', component: LoginComponent },
 
   // 3. Ruta para cargar la pantalla de registro
   { path: 'registro', component: Registro },
+
+  // 4. Ruta para cargar la pantalla de bienvenida
+  { path: 'bienvenida', component: BienvenidaComponent },
 ];
