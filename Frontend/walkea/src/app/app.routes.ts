2f53e051323c8272516b476afe11bd5f41e698cc
Rocky0524 <kenbalboa05@gmail.com>
2026-02-20 15:48:54 +0100

    login y registros
     pero tiene algunos errores

--- Frontend/walkea/src/app/app.routes.ts
+++ Frontend/walkea/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login'; 
+import { Registro } from './registro/registro'; 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  // 1. Ruta por defecto: Si el usuario entra a la raíz, va directo al login
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  
+  // 2. Ruta para cargar la pantalla de login
+  { path: 'login', component: LoginComponent },
+
+  // 3. Ruta para cargar la pantalla de registro
+  { path: 'registro', component: Registro },
+];
