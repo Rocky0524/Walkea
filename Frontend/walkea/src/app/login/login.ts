5f901237f36820045c910e5daf014b99ed6ebc82
Rocky0524 <kenbalboa05@gmail.com>
2026-02-24 16:19:58 +0100

    validadores para el login

--- Frontend/walkea/src/app/login/login.ts
+++ Frontend/walkea/src/app/login/login.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router'; 
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css' 
 })
 export class LoginComponent {
+loginForm = new FormGroup({
+email: new FormControl("",[Validators.required, Validators.email]),
+password: new FormControl("", [Validators.required, Validators.minLength(6)])
+})
+
+onSubmit(){
+  if (this.loginForm.valid){
+    console.log('Todo correcto. Listo para enviar a laravel:', this.loginForm.value);
+  }else {
+    console.log('Hay errores');
+    this.loginForm.markAllAsTouched();
+  }
+}
+
 
 }
