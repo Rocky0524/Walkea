869a34bf5d6adae71193d250f1443bea5bf63782
anasilerna <anasmejahad15@alumnes.ilerna.com>
2026-02-27 16:21:24 +0100

    arreglar config auth jwt y migracion tabla usuarios

--- backend/walkea/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/walkea/database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,20 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id('id_usuario');
+            $table->string('nombre');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->integer('reputacion')->default(0);
             $table->timestamps();
         });
 
@@ -42,7 +40,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('usuarios');
         Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
     }
