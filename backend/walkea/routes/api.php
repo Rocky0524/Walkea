99180eb8e80ca13f4ed2dc116eb67553a6f62f11
anasilerna <anasmejahad15@alumnes.ilerna.com>
2026-03-03 16:12:56 +0100

    Revert "crud marcador modelo y controller"
    This reverts commit 38e2a75e631fdab8494522b1cf971c07f63475b8.

--- backend/walkea/routes/api.php
+++ backend/walkea/routes/api.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TipoMarcadorController;
-use App\Http\Controllers\MarcadorController;
 
 // Rutas publicas
 Route::post('/register', [AuthController::class, 'register']);
@@ -11,8 +10,6 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/tipo-marcador', [TipoMarcadorController::class, 'index']);
 Route::get('/tipo-marcador/{id}', [TipoMarcadorController::class, 'show']);
-Route::get('/marcador', [MarcadorController::class, 'index']);
-Route::get('/marcador/{id}', [MarcadorController::class, 'show']);
 
 // Rutas protegidas con JWT
 Route::middleware('auth:api')->group(function () {
@@ -23,8 +20,4 @@ Route::middleware('auth:api')->group(function () {
     Route::post('/tipo-marcador', [TipoMarcadorController::class, 'store']);
     Route::put('/tipo-marcador/{id}', [TipoMarcadorController::class, 'update']);
     Route::delete('/tipo-marcador/{id}', [TipoMarcadorController::class, 'destroy']);
-
-    Route::post('/marcador', [MarcadorController::class, 'store']);
-    Route::put('/marcador/{id}', [MarcadorController::class, 'update']);
-    Route::delete('/marcador/{id}', [MarcadorController::class, 'destroy']);
 });
