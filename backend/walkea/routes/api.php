2f4fce7c928fbbff5529f8462374d86a6f520532
anasilerna <anasmejahad15@alumnes.ilerna.com>
2026-03-06 16:50:57 +0100

    crear controlador votaciones

--- backend/walkea/routes/api.php
+++ backend/walkea/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TipoMarcadorController;
+use App\Http\Controllers\MarcadorController;
+use App\Http\Controllers\VotoController;
 
 // Rutas publicas
 Route::post('/register', [AuthController::class, 'register']);
@@ -10,6 +12,9 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/tipo-marcador', [TipoMarcadorController::class, 'index']);
 Route::get('/tipo-marcador/{id}', [TipoMarcadorController::class, 'show']);
+
+Route::get('/marcador', [MarcadorController::class, 'index']);
+Route::get('/marcador/{id}', [MarcadorController::class, 'show']);
 
 // Rutas protegidas con JWT
 Route::middleware('auth:api')->group(function () {
@@ -20,4 +25,7 @@ Route::middleware('auth:api')->group(function () {
     Route::post('/tipo-marcador', [TipoMarcadorController::class, 'store']);
     Route::put('/tipo-marcador/{id}', [TipoMarcadorController::class, 'update']);
     Route::delete('/tipo-marcador/{id}', [TipoMarcadorController::class, 'destroy']);
+
+    Route::post('/marcador', [MarcadorController::class, 'store']);
+    Route::post('/marcador/{id}/votar', [VotoController::class, 'votar']);
 });
