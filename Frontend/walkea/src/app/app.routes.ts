facc92d06fcc94ac82cad57c1a394df6960bcc55
Rocky0524 <kenbalboa05@gmail.com>
2026-03-17 16:04:03 +0100

    hago commit para hacerme un merge

--- Frontend/walkea/src/app/app.routes.ts
+++ Frontend/walkea/src/app/app.routes.ts
@@ -2,18 +2,18 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login'; 
 import { RegistroComponent } from './registro/registro';
 import { BienvenidaComponent } from './bienvenida/bienvenida'; 
-
+import { MapaComponent } from './mapa/mapa'; 
 export const routes: Routes = [
-
-  // 1. Ruta por defecto: Si el usuario entra a la raíz, va directo al login
   { path: '', redirectTo: '/bienvenida', pathMatch: 'full' },
-  
-  // 2. Ruta para cargar la pantalla de login
   { path: 'login', component: LoginComponent },
-
-  // 3. Ruta para cargar la pantalla de registro
- { path: 'registro', component: RegistroComponent },
-
-  // 4. Ruta para cargar la pantalla de bienvenida
+  { path: 'registro', component: RegistroComponent },
   { path: 'bienvenida', component: BienvenidaComponent },
+  
+  { path: 'dashboard', component: MapaComponent },
+  
+  { 
+    path: 'mis-reportes', 
+    loadComponent: () => import('./mis-reportes/mis-reportes')
+      .then(m => m.MisReportesComponent) 
+  },
 ];
---
+++ Frontend/walkea/src/app/components/mis-reportes/mis-reportes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mis-reportes',
+  imports: [],
+  templateUrl: './mis-reportes.html',
+  styleUrl: './mis-reportes.css',
+})
+export class MisReportes {
+
+}
