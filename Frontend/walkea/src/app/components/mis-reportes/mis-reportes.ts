a7d35c334ce76a936400017d92f195d13aed82d0
Rocky0524 <kenbalboa05@gmail.com>
2026-03-18 16:04:08 +0100

    pagina mis reportes

--- Frontend/walkea/src/app/components/mis-reportes/mis-reportes.ts
+++ Frontend/walkea/src/app/components/mis-reportes/mis-reportes.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-mis-reportes',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule], 
   templateUrl: './mis-reportes.html',
   styleUrl: './mis-reportes.css',
 })
 export class MisReportes {
-
+  misReportes = [
+    { descripcion: 'Bache enorme', latitud: 41.61, longitud: 0.62, estado: 'activo', vida: 5 },
+    { descripcion: 'Farola fundida', latitud: 41.615, longitud: 0.622, estado: 'activo', vida: 8 }
+  ];
 }
