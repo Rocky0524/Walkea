36d14720b78f934aff8650592f62a2cec856c5ce
Rocky0524 <kenbalboa05@gmail.com>
2026-03-18 16:09:31 +0100

    datos y pruebas

--- Frontend/walkea/src/app/components/mis-reportes/mis-reportes.ts
+++ Frontend/walkea/src/app/components/mis-reportes/mis-reportes.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-mis-reportes',
   standalone: true,
-  imports: [CommonModule], 
+  imports: [CommonModule],
   templateUrl: './mis-reportes.html',
   styleUrl: './mis-reportes.css',
 })
 export class MisReportes {
+  // Estos datos luego vendrán del servicio de Anas vía API
   misReportes = [
-    { descripcion: 'Bache enorme', latitud: 41.61, longitud: 0.62, estado: 'activo', vida: 5 },
-    { descripcion: 'Farola fundida', latitud: 41.615, longitud: 0.622, estado: 'activo', vida: 8 }
+    { descripcion: 'Bache enorme en la entrada', latitud: 41.6177, longitud: 0.6267, estado: 'activo', vida: 5 },
+    { descripcion: 'Farola rota frente al parque', latitud: 41.6150, longitud: 0.6220, estado: 'activo', vida: 8 }
   ];
+
+  eliminarReporte(reporte: any) {
+    if(confirm('¿Estás seguro de que quieres eliminar este reporte?')) {
+      this.misReportes = this.misReportes.filter(r => r !== reporte);
+      console.log('Reporte eliminado localmente');
+    }
+  }
 }
