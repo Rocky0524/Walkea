79de9e979911561777a470589c502e6d100d6c39
Rocky0524 <kenbalboa05@gmail.com>
2026-03-19 16:34:31 +0100

    arreglo un poco los vavegadores y los estilos

--- Frontend/walkea/src/app/components/mis-reportes/mis-reportes.ts
+++ Frontend/walkea/src/app/components/mis-reportes/mis-reportes.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-mis-reportes',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterLink,RouterLinkActive],
   templateUrl: './mis-reportes.html',
   styleUrl: './mis-reportes.css',
 })
